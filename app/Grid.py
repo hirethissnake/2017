d04787351fa37175ad70781f182a91fbce7118c2
Daniel Frankcom <danielfrankcom@gmail.com>
2017-02-22 14:32:24 -0800

    Converted renderSnake.py to a class file
    Grid.py is now a general purpose class file with methods to do each of
    the previously accomplished tasks.

---
+++ app/Grid.py
@@ -0,0 +1,44 @@
+from appJar import gui
+from random import randint
+import colorsys
+
+class Grid:
+
+	def __init__(self, size):
+	
+		self.size = size # set grid size
+		self.grid = [[0 for i in range(size)] for j in range(size)] # generate a grid of size x size
+		
+		
+	def randomize(self, min, max):
+	
+		self.grid = [[randint(min, max) for i in range(self.size)] for j in range(self.size)] # fill grid with numbers between min and max
+	
+	
+	def show(self):
+	
+		app = gui('Login Window', '400x400')
+		app.setBg('white')
+		app.setTitle('SneakySnake Visualiser')
+
+		for i in range(self.size):
+			for k in range(self.size):
+				gridValue = self.grid[i][k]
+				
+				# interpolate square value from gridValue into HSV value between red and green, convert to RGB, convert to hex
+				hex = '#%02x%02x%02x' % tuple(i * 255 for i in colorsys.hls_to_rgb((gridValue * 1.2) / float(360), 0.6, 0.8))
+				
+				title = str(i) + "," + str(k)
+				app.addLabel(title, '', i, k)
+				app.setLabelBg(title, hex)
+
+		app.go()
+		
+		
+	def addBoundary(self, value):
+	
+		for i in range(self.size):
+			self.grid[0][i] = 0
+			self.grid[-1][i] = 0
+			self.grid[i][0] = 0
+			self.grid[i][-1] = 0
