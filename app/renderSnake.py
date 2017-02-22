9adc31499e8bb3902ba6095bc60f38ab291ab988
Daniel Frankcom <danielfrankcom@gmail.com>
2017-02-22 14:08:16 -0800

    Converted whitespace standard to tabs
    We will now use leading tabs instead of spaces for speed and
    accessibility

--- app/renderSnake.py
+++ app/renderSnake.py
@@ -4,12 +4,12 @@ import colorsys
 
 # function called by pressing the buttons
 def press(btn):
-    if btn == "Cancel":
-        app.stop()
-    if btn == 'Submit':
-        print 'User:', app.getEntry('user'), 'Pass:', app.getEntry('pass')
-    else:
-        app.infoBox('Help', 'Nah dawg, I can\'t help you.')
+	if btn == "Cancel":
+		app.stop()
+	if btn == 'Submit':
+		print 'User:', app.getEntry('user'), 'Pass:', app.getEntry('pass')
+	else:
+		app.infoBox('Help', 'Nah dawg, I can\'t help you.')
 
 app = gui('Login Window', '400x400')
 app.setBg('white')
@@ -30,21 +30,21 @@ app.setTitle('SneakySnake Visualiser')
 N = 20 # temp
 map_gradient = [[int(random.random()*100) for i in range(N)] for j in range(N)] # temp
 for i in range(N):
-    # add walls
-    map_gradient[0][i] = 100
-    map_gradient[-1][i] = 100
-    map_gradient[i][0] = 100
-    map_gradient[i][-1] = 100
+	# add walls
+	map_gradient[0][i] = 100
+	map_gradient[-1][i] = 100
+	map_gradient[i][0] = 100
+	map_gradient[i][-1] = 100
 
 for i in range(len(map_gradient)):
-    for k in range(len(map_gradient)):
-        l = map_gradient[i][k]
-        title = str(i) + "," + str(k)
-        
-        hex = '#%02x%02x%02x' % tuple(i * 255 for i in colorsys.hls_to_rgb((l * 1.2) / float(360), 0.6, 0.8)) #interpolate b/w red and green based on l, then convert to hex
+	for k in range(len(map_gradient)):
+		l = map_gradient[i][k]
+		title = str(i) + "," + str(k)
+		
+		hex = '#%02x%02x%02x' % tuple(i * 255 for i in colorsys.hls_to_rgb((l * 1.2) / float(360), 0.6, 0.8)) #interpolate b/w red and green based on l, then convert to hex
 
-        app.addLabel(title, '', i, k)
-        app.setLabelBg(title, hex)
+		app.addLabel(title, '', i, k)
+		app.setLabelBg(title, hex)
 
 
 app.go()
