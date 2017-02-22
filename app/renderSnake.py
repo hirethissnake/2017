055bfe2d2b8f1b16c483826a5ca59b7da06685d7
Phil D <Phil D>
2017-02-22 11:42:46 -0800

    Added colored rendering

--- app/renderSnake.py
+++ app/renderSnake.py
@@ -1,4 +1,5 @@
 from appJar import gui
+import random # temp
 
 # function called by pressing the buttons
 def press(btn):
@@ -9,7 +10,7 @@ def press(btn):
     else:
         app.infoBox('Help', 'Nah dawg, I can\'t help you.')
 
-# app = gui('Login Window', '400x200')
+app = gui('Login Window', '400x400')
 app.setBg('white')
 app.setFont(14, font='Segoe UI')
 app.setTitle('SneakySnake Visualiser')
@@ -24,11 +25,33 @@ app.setTitle('SneakySnake Visualiser')
 # app.setEntryFocus("user")
 # app.stopLabelFrame()
 
-for i in range(10):
-	for k in range(10):
-		title = str(i) + "," + str(k)
-		app.addLabel(title, ' ', i, k)
-		app.setLabelBg(title, 'yellow')
+# map is an NxN matrix of random values from 0 - 100
+N = 20 # temp
+map_gradient = [[int(random.random()*100) for i in range(N)] for j in range(N)] # temp
+for i in range(N):
+    # add walls
+    map_gradient[0][i] = 100
+    map_gradient[-1][i] = 100
+    map_gradient[i][0] = 100
+    map_gradient[i][-1] = 100
+
+for i in range(len(map_gradient)):
+    for k in range(len(map_gradient)):
+        l = map_gradient[i][k]
+    	title = str(i) + "," + str(k)
+        color = '#42b0f4' # Light blue, best possible score
+        if l > 10: 
+            color = '#41f47f' # Good, light green
+        if l > 30:
+            color = '#c1f441' # Average-Good, yellow-green
+        if l > 60:
+            color = '#f4eb41' # Average, yellow
+        if l > 80:
+            color = '#f49141' # Bad-average, orange
+        if l > 90:
+            color = '#f44341' # Bad, red
+        app.addLabel(title, '', i, k)
+        app.setLabelBg(title, color)
 
 
 app.go()
