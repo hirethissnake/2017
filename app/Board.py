34cb41172783d05d82b8190552891b58355c6820
Daniel Frankcom <danielfrankcom@gmail.com>
2017-02-26 00:11:14 -0800

    Added setWeight(), shortestPath() methods to Board
    These 2 functions are now fully functional, more to come

--- app/Board.py
+++ app/Board.py
@@ -2,6 +2,7 @@
     Includes vertices and edges."""
 import igraph
 
+
 class Board:
     """Store square weight and calculate optimal paths between them."""
 
@@ -15,27 +16,39 @@ class Board:
 
         for row in range(size):
             for col in range(size):
-                self.graph.add_edge(str(row) + ',' + str(col), str(row) + ',' +
-                str(col + 1), weight=1)
+                self.graph.add_edge(str(row) + ',' + str(col),
+                                    str(row) + ',' + str(col + 1), weight=1)
                 self.graph.add_edge(str(row) + ',' + str(col), str(row + 1) +
-                ',' + str(col), weight=1)
+                                    "," + str(col), weight=1)
+
+        for row in range(1, size + 1):
+            for col in range(1, size + 1):
+                self.graph.add_edge(str(row) + ',' + str(col), str(row) + ',' +
+                                    str(col - 1), weight=1)
+                self.graph.add_edge(str(row) + ',' + str(col), str(row - 1) +
+                                    "," + str(col), weight=1)
+
+        """shortest = self.shortestPath("0,1", "2,5")
 
         print self.graph
+        print shortest
+        for vertex in shortest:
+            print self.graph.vs.find(vertex)"""
 
     def shortestPath(self, u, v):
         """Return shortest path between nodes u and v."""
-        return self.graph.get_shortest_paths(u, to=v, weight="weight")
+        return self.graph.get_shortest_paths(u, to=v, weights="weight",
+                                             mode="OUT", output="vpath")[0]
 
-    """
+    def setWeight(self, u, weight):
+        """Set incoming edges of vertex u to weight"""
+        edges = self.graph.incident(self.graph.vs.find(u))
+        for edge in edges:
+            self.graph.es.find(edge)["weight"] = weight
+        print self.graph.es["weight"]
 
-    isDirected()
-    shortestPath(u,v)
-    isPathPossible()
-    addEdge()
-    addVertex()
-    ...etc
-
-    """
 
 if __name__ == '__main__':
+    """Run code in this class for testing"""
     g = Board(20)
+    g.setWeight("1,1", 2)
