f560ba79f38a462174b56f796cdb95d5d4614c77
Phil Denhoff <phildenhoff@gmail.com>
2017-03-01 10:43:52 -0800

    Wrote Update tests for Snake.py
    Tests valid and invalid updates

--- app/Test.py
+++ app/Test.py
@@ -6,13 +6,13 @@
 
 from Snake import Snake
 
-NUM_CASES = 10
+NUM_CASES = 13
 current_case = 0
 
 def Snake_test_1():
     """Test basic .getX and .setX functionality."""
     print 'Testing .getX using init'
-    init_params = [2, [[0, 1], [1, 1]], 75, 't1'] # pylint: disable=W0612
+    init_params = [2, [[0, 1], [1, 1]], 75, 's1']
     s1 = Snake(init_params[0], init_params[1], init_params[2], init_params[3])
 
     # test .toString()
@@ -27,6 +27,8 @@ positions: " + str(init_params[1]), 0)
     testCase(s1.getSize(), init_params[0], 3)
     testCase(s1.getHeadPosition(), init_params[1][0], 4)
     testCase(s1.getAllPositions(), init_params[1], 5)
+    # not implelemented yet
+    # testCase(s1.getFoodUrgency(), X, num)
 
     print 'Testing .setState'
     # a) valid state
@@ -38,7 +40,31 @@ positions: " + str(init_params[1]), 0)
         testCase('absolutely', 'terribly wrong', 7)
     except ValueError:
         testCase(1, 1, 7)
+    del s1
 
+    Snake_test_2()
+
+def Snake_test_2():
+    """Update tests."""
+    print 'Testing update'
+    init_params = [3, [[0, 1], [1, 1], [1, 2]], 66, 's2']
+    s2 = Snake(init_params[0], init_params[1], init_params[2], init_params[3])
+
+    # valid updates
+    s2.update([1, 0], 'false', init_params[2]-1)
+    testCase(s2.getHeadPosition(), [1, 0], 8)
+    testCase(s2.getHealth(), init_params[2]-1, 9)
+    testCase(s2.getSize(), init_params[0], 10)
+
+    s2.update([2, 0], 'true', init_params[2]-2)
+    testCase(s2.getSize(), init_params[0]+1, 11)
+
+    # invalid updates
+    try:
+        s2.update([3, 0], 'true', 'dog')
+        testCase('nope', 'failed test', 12)
+    except ValueError:
+        testCase(1, 1, 12)
 
 
 def testCase(var1, var2, testNum):
