328aef8f8e72c1932295d1d64c7d386be1b75bd6
Phil Denhoff <phildenhoff@gmail.com>
2017-03-01 12:36:36 -0800

    Removed foodBoolean and improved testCase()
    - foodBoolean removed. update() now tests if new health is greater than
    old_health, if so, snake length++, otherwise the last position value is
    popped.
    - getIdentifier() added - returns snake's UUID
    - converted tests from identification via an int to a string, so it's
    more clear which test failed, and new tests can be added in between
    cases

--- app/Test.py
+++ app/Test.py
@@ -6,12 +6,19 @@
 
 from Snake import Snake
 
-NUM_CASES = 13
+
+num_cases = 0
 current_case = 0
 
+
 def Snake_test_1():
-    """Test basic .getX and .setX functionality."""
+    """Test basic .getX and .setX functionality.
+    This test suite has 9 tests.
+    """
     print 'Testing .getX using init'
+    global num_cases
+    num_cases += 9
+
     init_params = [2, [[0, 1], [1, 1]], 75, 's1']
     s1 = Snake(init_params[0], init_params[1], init_params[2], init_params[3])
 
@@ -21,28 +28,28 @@ def Snake_test_1():
 health: " + str(init_params[2]) + "\n\
 size: " + str(init_params[0]) + "\n\
 state: unknown\n\
-positions: " + str(init_params[1]), 0)
-    testCase(s1.getState(), 'unknown', 1)
-    testCase(s1.getHealth(), init_params[2], 2)
-    testCase(s1.getSize(), init_params[0], 3)
-    testCase(s1.getHeadPosition(), init_params[1][0], 4)
-    testCase(s1.getAllPositions(), init_params[1], 5)
+positions: " + str(init_params[1]), 'toString')
+    testCase(s1.getState(), 'unknown', 'getState')
+    testCase(s1.getHealth(), init_params[2], 'getHealth')
+    testCase(s1.getSize(), init_params[0], 'getSize')
+    testCase(s1.getHeadPosition(), init_params[1][0], 'getHeadPosition')
+    testCase(s1.getAllPositions(), init_params[1], 'getAllPositions')
+    testCase(s1.getIdentifier(), init_params[3], 'getIdentifier')
     # not implelemented yet
     # testCase(s1.getFoodUrgency(), X, num)
 
     print 'Testing .setState'
     # a) valid state
     s1.setState('food')
-    testCase(s1.getState(), 'food', 6)
+    testCase(s1.getState(), 'food', 'valid setState')
     # b) invalid state
     try:
         s1.setState('watermelon')
-        testCase('absolutely', 'terribly wrong', 7)
+        testCase('absolutely', 'terribly wrong', 'invalid setState')
     except ValueError:
-        testCase(1, 1, 7)
+        testCase(1, 1, 'invalid setState')
     del s1
 
-    Snake_test_2()
 
 def Snake_test_2():
     """Update tests."""
@@ -51,34 +58,40 @@ def Snake_test_2():
     s2 = Snake(init_params[0], init_params[1], init_params[2], init_params[3])
 
     # valid updates
-    s2.update([1, 0], 'false', init_params[2]-1)
-    testCase(s2.getHeadPosition(), [1, 0], 8)
-    testCase(s2.getHealth(), init_params[2]-1, 9)
-    testCase(s2.getSize(), init_params[0], 10)
+    s2.update([1, 0], init_params[2]-1)
+    testCase(s2.getHeadPosition(), [1, 0], 'getHeadPosition after update')
+    testCase(s2.getHealth(), init_params[2]-1, 'getHealth after update')
+    testCase(s2.getSize(), init_params[0], 'getSize after update')
 
-    s2.update([2, 0], 'true', init_params[2]-2)
-    testCase(s2.getSize(), init_params[0]+1, 11)
+    s2.update([2, 0], 100)
+    testCase(s2.getSize(), init_params[0]+1, 'getSize after update')
 
     # invalid updates
     try:
-        s2.update([3, 0], 'true', 'dog')
-        testCase('nope', 'failed test', 12)
+        s2.update([3, 0], 'dog')
+        testCase('nope', 'failed test', 'invalid update')
     except ValueError:
-        testCase(1, 1, 12)
+        testCase(1, 1, 'invalid update')
 
 
-def testCase(var1, var2, testNum):
+def testCase(var1, var2, testIdent):
     """Run comparison tests, and if they fail, will raise an exception."""
     global current_case
     if var1 != var2:
-        raise ValueError('FAILED TEST ' + str(testNum))
+        errStr = str('FAILED TEST for %s. Completed %i of %i tests.'\
+                    % (str(testIdent), current_case, num_cases))
+        raise ValueError(errStr)
     current_case += 1
 
-# Snake.py tests
-print '-- Testing Snake.py --'
-try:
-    Snake_test_1()
-    print "Test completed successfully. Passed " + str(current_case) + \
-     " of " + str(NUM_CASES) + " test cases."
-except ValueError as failure:
-    print failure
+
+if __name__ == '__main__':
+    num_cases = 0
+    # Snake.py tests
+    print '-- Testing Snake.py --'
+    try:
+        Snake_test_1()
+        Snake_test_2()
+        print "Test completed successfully. Passed " + str(current_case) + \
+         " of " + str(num_cases) + " test cases."
+    except ValueError as failure:
+        print failure
