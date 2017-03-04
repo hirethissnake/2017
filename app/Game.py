cde41bbedc80103ee5ff9c051ae1901a61c646ef
Kyle Thorpe <kylejohnthorpe@gmail.com>
2017-03-04 15:47:24 -0800

    Fixed Return

--- app/Game.py
+++ app/Game.py
@@ -314,10 +314,11 @@ class Game:
         for other_opt in otherOptions:
             if self.weightGrid.optimumPathLength(other_opt, u) == float('inf'):
                 dont = True
+        self.weightGrid.setWeights(n, 1)
         if dont:
             print "Switched directions from weightEnclosedSpaces"
             return otherOptions[0]
-        self.weightGrid.setWeights(n, 1)
+        return u
         #set other snak eotpions to 1
 
 
